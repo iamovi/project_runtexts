fb6bba9b57392c1d8cb7f452a537f6539fcda0e1
Maruf Ovi <imgoinin.iamovi@gmail.com>
2024-11-11 18:05:00 +0600

    release 1.2

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name="runtexts",
-    version="1.0",
+    version="1.2",
     packages=find_packages(),
-    install_requires=["pyautogui"],
+    install_requires=[
+        "pyautogui",
+        "colorama",
+        "requests",
+    ],
     entry_points={
         "console_scripts": [
             "runtexts=runtexts.cli:main",
         ],
     },
     author="Maruf Ovi",
+    author_email="fornet.ovi@gmail.com",
     description="A CLI tool to send automated messages.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/iamovi/project_runtexts",
+    license="MIT",
+    keywords="automation, messaging, CLI, pyautogui",
+    python_requires=">=3.6",
 )
